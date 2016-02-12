a4e46fe7bef4bf7428e6116984eb4f5c1bbc886e
Taylor Andrews <taan7753@colorado.edu>
2016-02-11 17:41:03 -0700

    Added a setup file

---
+++ setup.py
@@ -0,0 +1,17 @@
+# GitCU Data Mining Project Spring 2016
+# Created by: Taylor Andrews
+# Modified: 2016/02/11 
+#
+# Setup directories for the project.
+
+import os
+
+def main():
+    required_directories = ['./data/classes']
+
+    for d in required_directories:
+        if not os.path.exists(d):
+            os.makedirs(d)
+
+if __name__ == '__main__':
+    main()
