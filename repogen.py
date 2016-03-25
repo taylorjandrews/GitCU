1f27fd200cf65a9ad6e394b6950daf538cc3c273
Taylor Andrews <taylor.andrews-1@colorado.edu>
2016-03-24 23:14:09 -0600

    Generate repository data from user list

---
+++ repogen.py
@@ -0,0 +1,59 @@
+"""GitCU Data Mining Project Spring 2016
+   Created by: Taylor Andrews
+   Modified: 2016/03/24 
+
+   Script to generate a list of repositories from a list of 
+   GitHub users.
+"""
+import requests
+import json
+import csv
+from time import sleep
+
+def get_repositories(user):
+    """Given a GitHub user, query Github for corresponding repositories.
+
+    Args:
+        user: a user to scan the Github repositories of.
+
+    Returns:
+        A list of json data about Github repositories corresponding to the desired user. 
+    """
+    print("Mining {}...".format(user), end='', flush=True) 
+        
+    req_string = "https://api.github.com/users/" + user + "/repos"
+    r = requests.get(req_string).json()
+
+    sleep(60)
+    print("finished!!!")
+
+    return r
+
+def store_repositories(user, data):
+    """Given a specific user and information about the repositories owned by that user,
+       store that data in a file.    
+
+    Args:
+        user: a specific user to record information about.
+        data: json data about Github repositories.
+    """
+    filename = "./data/repos/gitCU-user-" + user + ".json"
+    
+    with open(filename, 'wb') as f:
+        f.write(bytes(json.dumps(data, indent=2), 'UTF-8'))
+
+def main():
+	user_list = []
+
+	with open("./data/gitCU-users.csv") as f:
+		reader = csv.reader(f)
+		next(reader) # Skip the first line
+		for row in reader:
+			user_list.append(row[0])
+
+	for user in user_list:
+		repos = get_repositories(user)
+		store_repositories(user, repos)
+
+if __name__ == '__main__':
+    main()
