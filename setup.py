3b1f58e934257b30ac6f36aeb779c868882012ff
Taylor Andrews <taylor.andrews-1@colorado.edu>
2016-03-31 20:01:16 -0600

    Modified setup to make a folder for commits

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 import os
 
 def main():
-    required_directories = ['./data/classes', './data/repos']
+    required_directories = ['./data/classes', './data/repos', './data/commits']
 
     for d in required_directories:
         if not os.path.exists(d):
