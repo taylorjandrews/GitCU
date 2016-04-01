2e6c2b5da2e57a73ff2f8f2b4cf9831cc7a9e2ca
Taylor Andrews <taylor.andrews-1@colorado.edu>
2016-03-31 20:00:49 -0600

    Added file to fetch github commit information

---
+++ commitgen.py
@@ -0,0 +1,73 @@
+"""GitCU Data Mining Project Spring 2016
+   Created by: Taylor Andrews
+   Modified: 2016/03/24 
+
+   Script to generate commits from repositories.
+"""
+import requests
+import json
+import csv
+import os
+from time import sleep
+
+def get_commit_urls(data):
+    """Given GitHub repository data for a user, fetch the commit urls.
+
+    Args:
+        data: a json file containing repository information
+
+    Returns:
+        A list of commit urls to fetch more information about. 
+    """
+    commits = []
+
+    for repo in data:
+        if 'commits_url' in repo:
+            commits.append(repo['commits_url'])
+
+    return commits
+
+def create_userdir(username):
+    dirname = './data/commits/' + username
+    if not os.path.exists(dirname):
+        os.makedirs(dirname)
+
+    return dirname + '/'
+
+def fetch_commits(user, data):
+    dirname = create_userdir(user)
+
+    for commit in data:
+        url = commit.split('/')[5]
+        print("Mining {}/{}...".format(user, url), end='', flush=True) 
+
+        r = requests.get(commit.split('{')[0]).json()
+        store_commit(dirname, url, r)
+
+        sleep(60)
+        print("finished!!!")
+
+
+def store_commit(dirname, url, commit):
+    """Given a specific directory, store commit information in that directory.    
+
+    Args:
+        dir: the name of a directory to store information in.
+        url: part of the url of the commit to use as a filename.
+        commit: the data corresponding to a specific GitHub commit.
+    """
+    filename = dirname + url + ".json"
+    
+    with open(filename, 'wb') as f:
+        f.write(bytes(json.dumps(commit, indent=2), 'UTF-8'))
+
+def main():
+    for f in os.listdir('./data/repos'):
+        with open('./data/repos/' + f, 'r') as repo:
+            data = json.load(repo)	
+            username = '-'.join(repo.name.split('-')[2:]).split('.')[0]
+            commits = get_commit_urls(data)
+            fetch_commits(username, commits)
+
+if __name__ == '__main__':
+    main()
