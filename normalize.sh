332f9a9fa89cd0aca7ac5eabe3025ea74ed14fe4
Taylor Andrews <taylor.andrews-1@colorado.edu>
2016-04-28 21:59:32 -0600

    Added normalize script

---
+++ normalize.sh
@@ -0,0 +1,6 @@
+python Andrews_Taylor_HW2.py -f1 ../project/GitCU/data/result-githubrepo.csv -a1 ' stars' -n min_max > ../project/GitCU/data/stars_norm.txt
+python Andrews_Taylor_HW2.py -f1 ../project/GitCU/data/result-githubrepo.csv -a1 ' forks' -n min_max > ../project/GitCU/data/forks_norm.txt
+python Andrews_Taylor_HW2.py -f1 ../project/GitCU/data/result-githubrepo.csv -a1 ' forked_from' -n min_max > ../project/GitCU/data/forked_from_norm.txt
+python Andrews_Taylor_HW2.py -f1 ../project/GitCU/data/result-githubrepo.csv -a1 ' watchers' -n min_max > ../project/GitCU/data/watchers_norm.txt
+python Andrews_Taylor_HW2.py -f1 ../project/GitCU/data/result-githubrepo.csv -a1 ' avg_size' -n min_max > ../project/GitCU/data/size_norm.txt
+
